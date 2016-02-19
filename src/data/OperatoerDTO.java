f4cd5567d80d9f1dc4dfd6de580dd764797b4a35
MikkelLeth <MikkelLeth@mikkels-mbp-2.home>
2016-02-19 11:33:24 +0100

    OperatoerDTO Added getters and setters

--- src/data/OperatoerDTO.java
+++ src/data/OperatoerDTO.java
@@ -1,9 +1,49 @@
 package data;
 
 public class OperatoerDTO {
-    int    oprId;                     
-    String oprNavn;                
-    String ini;   //Initialer               
-    int    cpr;                 
-    String password; 
+    private int oprId;                     
+    private String oprNavn;                
+    private String ini;   //Initialer               
+    private int cpr;                 
+    private String password;
+    
+    public int getID(){
+    	return oprId;
+    }
+    
+    public String getNavn() {
+    	return oprNavn;
+    }
+    
+    public String getIni() {
+    	return ini;
+    }
+    
+    public int getCpr() {
+    	return cpr;
+    }
+    
+    public String getPassword(){
+    	return password;
+    }
+    
+    public void setID(int newId){
+    	oprId = newId;
+    }
+    
+    public void setNavn(String navn){
+    	oprNavn = navn;
+    }
+    
+    public void setIni(String ini){
+    	this.ini = ini;
+    }
+    
+    public void setCpr(int cpr){
+    	this.cpr = cpr;
+    }
+    
+    public void setPassword(String password){
+    	this.password = password;
+    }
 }
