6f8232e64b4d4623dc7efa29c31a3909cd9b21d6
Hjorthen <Hjorthensebastian@gmail.com>
2016-02-19 13:23:06 +0100

    Added additional methods to the IGUI

--- src/interfaces/IGUI.java
+++ src/interfaces/IGUI.java
@@ -6,4 +6,7 @@ public interface IGUI {
 	public String getName();
 	public String getIni();
 	public String getCpr();
+	public void showMessage(String message);
+	public String getString();
+	public int getUserSelection(String... strings);
 }
