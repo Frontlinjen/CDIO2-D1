f1a4cba779224c0c1019591022b67b250bf66e6c
Hjorthen <Hjorthensebastian@gmail.com>
2016-02-19 12:21:19 +0100

    Added GUI interface

---
+++ src/interfaces/IGUI.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface IGUI {
+	public String getPassword();
+	public int getUID();
+	public String getName();
+	public String getIni();
+	public String getCpr();
+}
