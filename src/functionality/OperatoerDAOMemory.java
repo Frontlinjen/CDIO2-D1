7a59fac89a2a61882ad17d8734c00799c3d1226b
Senad Begovic <Senadbegovic2500@hotmail.com>
2016-02-19 11:53:04 +0100

    Created class OperatoerDAOMemory

---
+++ src/functionality/OperatoerDAOMemory.java
@@ -0,0 +1,39 @@
+package functionality;
+
+import java.util.List;
+
+import data.OperatoerDTO;
+
+public class OperatoerDAOMemory implements IOperatoerDAO{
+
+	@Override
+	public OperatoerDTO getOperatoer(int oprId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OperatoerDTO> getOperatoerList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createOperatoer(OperatoerDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateOperatoer(OperatoerDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteOperatoer(OperatoerDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
