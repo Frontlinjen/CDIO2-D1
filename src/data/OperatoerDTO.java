7962140abeb9c0fdc66e53e39458bc69d1db0ae2
Hjorthen <Hjorthensebastian@gmail.com>
2016-02-19 11:20:37 +0100

    Added the operatoerDTO class

---
+++ src/data/OperatoerDTO.java
@@ -0,0 +1,9 @@
+package data;
+
+public class OperatoerDTO {
+    int    oprId;                     
+    String oprNavn;                
+    String ini;   //Initialer               
+    int    cpr;                 
+    String password; 
+}
