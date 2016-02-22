01daa45e0101258fb4ac4204bc88cd2fbff53f45
SenadBegovic <senadbegovic2500@hotmail.com>
2016-02-22 15:26:22 +0100

    Fixed functions in OperatoerDAOMemory

--- src/functionality/OperatoerDAOMemory.java
+++ src/functionality/OperatoerDAOMemory.java
@@ -1,41 +1,50 @@
-package functionality;
+ package functionality;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import data.IOperatoerDTO;
-import data.OperatoerDTO;
 
 public class OperatoerDAOMemory implements IOperatoerDAO{
 
-	private IOperatoerDTO data;
-	
-	public OperatoerDAOMemory(IOperatoerDTO data){
-		this.data = data;
-	}
+	private List<IOperatoerDTO> data = new ArrayList<IOperatoerDTO>();
+
 	
 	@Override
-	public OperatoerDTO getOperatoer(int oprId) throws DALException {
+	public IOperatoerDTO getOperatoer(int oprId) throws DALException {
+		for (IOperatoerDTO operator : data) {
+			if(operator.getID()==oprId)
+			{
+				return operator;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public List<IOperatoerDTO> getOperatoerList() throws DALException {
-		return null;
+		return data;
 	}
 
 	@Override
 	public void createOperatoer(IOperatoerDTO opr) throws DALException {
-
+		data.add(opr);
 	}
 
 	@Override
 	public void updateOperatoer(IOperatoerDTO opr) throws DALException {
-
+		for (int i = 0; i < data.size(); i++) {
+			if(opr.getID()==data.get(i).getID())
+			{
+				data.remove(i);
+				data.add(opr);
+			}
+		}
 	}
 
 	@Override
 	public void deleteOperatoer(IOperatoerDTO opr) throws DALException {
-
+		data.remove(opr);
 	}
 
 }
